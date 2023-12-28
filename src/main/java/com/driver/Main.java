467d9dd8e3937b447f8ca754216ee9cea90e5c45
pkMAHTOZee <86905216+pkMAHTOZee@users.noreply.github.com>
2023-12-28 16:00:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,10 +5,6 @@ public class Main {
         // Task 3: Create an object of class B and call method meth of class A with it
         B objB = new B();
         String resultTask3 = objB.meth();
-        System.out.println(resultTask3);
-
-        // Task 5: Call the overridden method from obj of class B
-        String resultTask5 = objB.meth();
-        System.out.println(resultTask5);
+        
     }
 }
